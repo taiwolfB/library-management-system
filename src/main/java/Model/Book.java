42a95ae2122731f88379d662784ef2d982e55801
Bogdan Tailup <bogdan.tailup@mc3lab.com>
2021-11-01 00:02:29 +0200

    Initil setup

--- src/main/java/Model/Book.java
+++ src/main/java/Model/Book.java
@@ -1,4 +1,87 @@
 package Model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
 public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+    private String isbn;
+    private UUID uuid;
+    private UUID borrowedBy;
+    private LocalDateTime borrwedDate;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getBorrowedBy() {
+        return borrowedBy;
+    }
+
+    public void setBorrowedBy(UUID borrowedBy) {
+        this.borrowedBy = borrowedBy;
+    }
+
+    public LocalDateTime getBorrwedDate() {
+        return borrwedDate;
+    }
+
+    public void setBorrwedDate(LocalDateTime borrwedDate) {
+        this.borrwedDate = borrwedDate;
+    }
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String genre, String isbn, UUID uuid, UUID borrowedBy, LocalDateTime borrwedDate) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isbn = isbn;
+        this.uuid = uuid;
+        this.borrowedBy = borrowedBy;
+        this.borrwedDate = borrwedDate;
+    }
 }
--- src/main/java/Model/Student.java
+++ src/main/java/Model/Student.java
@@ -1,4 +1,53 @@
 package Model;
 
+import java.util.UUID;
+
 public class Student {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private boolean blacklisted;
+    private String email;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isBlacklisted() {
+        return blacklisted;
+    }
+
+    public void setBlacklisted(boolean blacklisted) {
+        this.blacklisted = blacklisted;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
