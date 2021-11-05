1f21617767f3472a0b0aab8ef40ed6259e385ff0
Cristian Anitei <cristiana.inventiff@gmail.com>
2021-11-05 20:01:08 +0000

    Finish interface implementation. patch

---
+++ src/main/java/org/team4/libraryManagement/Presentation/MainController.java
@@ -0,0 +1,15 @@
+package org.team4.libraryManagement.Presentation;
+
+import com.jfoenix.controls.JFXDialog;
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import org.team4.libraryManagement.Presentation.Dialogs.DialogService;
+
+public class MainController {
+
+    @FXML StackPane root;
+
+    @FXML void initialize() {
+    }
+
+}
